28e129246300c7cc48326d6c4954ce77eef90223
Dushyant Panchal <dushyant18033@iiitd.ac.in>
2020-04-24 14:13:34 +0530

    break into frames and frames form the stream

---
+++ Peer-Server.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({
+  port: 9000
+  /*ssl: {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+  }*/
+});
