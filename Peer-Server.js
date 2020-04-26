7006800b1c860ba7b08213551d6709d5b8ae1c68
Dushyant Panchal <dushyant18033@iiitd.ac.in>
2020-04-26 11:31:14 +0530

    added audio

--- Peer-Server.js
+++ Peer-Server.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const { PeerServer } = require('peer');
 
 const peerServer = PeerServer({
-  port: 9000
-  /*ssl: {
+  port: 9000,
+  ssl: {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
-  }*/
+  }
 });
--- app.py
+++ app.py
@@ -101,5 +101,5 @@ def logout():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    # app.run(host='0.0.0.0', debug=True, ssl_context=('cert.pem', 'key.pem'))
-    app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', debug=True, ssl_context=('cert.pem', 'key.pem'))
+    # app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
